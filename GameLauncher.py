6e73b959064df598d79aa2a0e690a8d3fca462a8
gomezja <jgomez42gb@gmail.com>
2019-07-03 13:38:55 -0700

    players are able to place their marker on the board

--- GameLauncher.py
+++ GameLauncher.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Player 1 chooses their marker.
 
@@ -13,6 +15,18 @@ def player_input():
         return ('X', 'O')
     else:
         return ('O', 'X')
+
+"""
+    Function uses random int (0 or 1) to decide which player goes first.
+
+    :returns: Player who goes first.
+"""
+def choose_first():
+    if random.randint(0,1):
+        return "Player 1"
+    else:
+        return "Player 2"
+
 """
 Clears screen between board updates.
 """
@@ -39,7 +53,38 @@ def display_board(board):
     print("  " + board[1] + " | " + board[2] + " | " + board[3] + "  ")
     print("    |   |    ")
 
-    print("\nPlayer 1: {}, Player 2: {}".format(player1_mark, player2_mark))
+"""
+Updates the board list with the new marker.
+
+:param board: A list of the current board.
+:param marker: The marker being placed.
+:param position: The position being updated.
+"""
+def place_marker(board, marker, position):
+    board[position] = marker
+
+"""
+The function validates a players chosen move.
+
+:param board: A list of the current game board.
+:param turn: The current player choosing move.
+:returns: A valid player move.
+"""
+def player_choice(board, turn):
+    position = 0
+
+    while position not in range(1,10) or not space_check(board, position):
+        position = int(input(str(turn) + " enter your move: "))
+
+    return position
+
+"""
+Checks if board position is available.
+
+:returns: A boolean indicating space availability.
+"""
+def space_check(board, position):
+    return board[position] == " "
 
 print("Welcome to Tic Tac Toe!\n")
 
@@ -49,4 +94,19 @@ player1_mark, player2_mark = player_input()
 # create initial board
 board = [' '] * 10
 
+turn = choose_first()
+
+game_on = True
 display_board(board)
+
+while game_on:
+    if turn == "Player 1":
+        place_marker(board, player1_mark, player_choice(board, turn))
+        display_board(board)
+
+        turn = "Player 2"
+    else:
+        place_marker(board, player2_mark, player_choice(board, turn))
+        display_board(board)
+
+        turn = "Player 1"
