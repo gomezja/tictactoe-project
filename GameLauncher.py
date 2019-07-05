73f6872d13100f911a2c693c5bc405a7648c767c
gomezja <jgomez42gb@gmail.com>
2019-07-05 14:51:43 -0700

    added game replay and full board check

--- GameLauncher.py
+++ GameLauncher.py
@@ -37,21 +37,26 @@ def clear_output():
 Displays the current game board.
 
 :param board: A list with all the current markers on the board.
+:param player1_mark: The marker for player one.
+:param player2_mark: The marker for player two.
 """
-def display_board(board):
+def display_board(board, player1_mark, player2_mark):
     clear_output()
 
-    print("    |   |    ")
-    print("  " + board[7] + " | " + board[8] + " | " + board[9] + "  ")
-    print("    |   |    ")
-    print("-------------")
-    print("    |   |    ")
-    print("  " + board[4] + " | " + board[5] + " | " + board[6] + "  ")
-    print("    |   |    ")
-    print("-------------")
-    print("    |   |    ")
-    print("  " + board[1] + " | " + board[2] + " | " + board[3] + "  ")
-    print("    |   |    ")
+    print("   Tic Tac Toe\n")
+    print("     |     |     ")
+    print("  " + board[7] + "  |  " + board[8] + "  |  " + board[9] + "  ")
+    print("     |     |     ")
+    print("-----------------")
+    print("     |     |     ")
+    print("  " + board[4] + "  |  " + board[5] + "  |  " + board[6] + "  ")
+    print("     |     |     ")
+    print("-----------------")
+    print("     |     |     ")
+    print("  " + board[1] + "  |  " + board[2] + "  |  " + board[3] + "  ")
+    print("     |     |     ")
+    print("\nPlayer 1 = " + str(player1_mark))
+    print("Player 2 = " + str(player2_mark) + "\n")
 
 """
 Updates the board list with the new marker.
@@ -74,39 +79,96 @@ def player_choice(board, turn):
     position = 0
 
     while position not in range(1,10) or not space_check(board, position):
-        position = int(input(str(turn) + " enter your move: "))
+        try:
+            position = int(input(str(turn) + " enter your move (1-9): "))
+        except ValueError:
+            pass
 
     return position
 
 """
 Checks if board position is available.
 
+:param board: A list of the current board.
+:param position: The player's target space.
 :returns: A boolean indicating space availability.
 """
 def space_check(board, position):
     return board[position] == " "
 
+"""
+Checks if the board is completely full.
+
+:param board: A list of the current board.
+:returns: A boolean indicating if board is full.
+"""
+def full_board_check(board):
+    for i in range(1, 10):
+        if space_check(board, i):
+            return False
+    return True
+
+"""
+Asks a question based on string parameter.
+
+:param text: The string for the question being asked.
+:returns: A boolean indicating the answer to the question.
+"""
+def check_play(text):
+    choice = ''
+
+    while not(choice == 'Y' or choice == 'N'):
+        choice = input(str(text) + " (Y/N)? ").upper()
+
+    return choice == 'Y'
+
+#========================================
+
 print("Welcome to Tic Tac Toe!\n")
 
-# set player markers
-player1_mark, player2_mark = player_input()
+while True:
+    # set player markers
+    player1_mark, player2_mark = player_input()
 
-# create initial board
-board = [' '] * 10
+    # create initial board
+    board = [' '] * 10
 
-turn = choose_first()
+    # choose player that is going first
+    print("\nChoosing player to go first...")
+    turn = choose_first()
+    print(str(turn) + " will go first.\n")
 
-game_on = True
-display_board(board)
+    # check if players are ready
+    while not check_play("Ready to play"):
+        if check_play("Exit game"):
+            raise SystemExit(0)
 
-while game_on:
-    if turn == "Player 1":
-        place_marker(board, player1_mark, player_choice(board, turn))
-        display_board(board)
+    # active game
+    while True:
+        if turn == "Player 1":
+            display_board(board, player1_mark, player2_mark)
+            place_marker(board, player1_mark, player_choice(board, turn))
 
-        turn = "Player 2"
-    else:
-        place_marker(board, player2_mark, player_choice(board, turn))
-        display_board(board)
+            if full_board_check(board):
+                display_board(board, player1_mark, player2_mark)
 
-        turn = "Player 1"
+                print("The game is a draw!")
+                break
+            else:
+                turn = "Player 2"
+        else:
+            display_board(board, player1_mark, player2_mark)
+            place_marker(board, player2_mark, player_choice(board, turn))
+
+            if full_board_check(board):
+                display_board(board, player1_mark, player2_mark)
+
+                print("The game is a draw!")
+                break
+            else:
+                turn = "Player 1"
+
+    if not check_play("\nPlay again"):
+        raise SystemExit(0)
+
+    clear_output()
